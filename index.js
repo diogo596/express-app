bc816e528113f212b555c3102f917cf434d1516c
diogo596 <diogo596@hotmail.fr>
2023-11-27 22:38:28 +0100

    test ok

--- index.js
+++ index.js
@@ -1,51 +1,6 @@
-const express = require('express');
-
-const app = express();
+const app = require('./app');
 
 const port = 7000;
-
-app.get('/', (req, res) => {
-  res.send('Welcome to my favourite movie list');
-});
-
-const movies = [
-  {
-    id: 1,
-    title: 'Citizen Kane',
-    director: 'Orson Wells',
-    year: '1941',
-    color: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: 'The Godfather',
-    director: 'Francis Ford Coppola',
-    year: '1972',
-    color: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: 'Pulp Fiction',
-    director: 'Quentin Tarantino',
-    year: '1994',
-    color: true,
-    duration: 180,
-  },
-];
-
-const getMovies = (req, res) => {
-  res.status(200).json(movies);
-};
-
-app.get('/api/movies', getMovies);
-
-app.get('/api/movies/:id', (req, res) => {
-  const movie = movies.find((c) => c.id === parseInt(req.params.id));
-  if (!movie) res.status(404).send('Not Found');
-  res.send(movie);
-});
 
 app
   .listen(port, () => {
